a48f6682a9a5ef66605243690a53a55b578e40b7
Harold Rodriguez <harold_r008@hotmail.com>
2019-03-28 19:37:57 -0500

    primer_envio

---
+++ movimiento/Assets/script/Control.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+
+    public Animator anim;
+    public float WalkSpeed;
+    // Update is called once per frame
+    void Update(){
+        if (Input.GetKey(KeyCode.W)){
+            anim.SetBool("Walk", true);
+            transform.Translate(Vector3.forward * WalkSpeed * Time.deltaTime);
+        }
+        else
+        {
+            anim.SetBool("Walk", false);
+        }
+        
+    }
+}
